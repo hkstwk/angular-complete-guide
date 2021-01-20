d5efdd62434dcf88617b5985d130f498d785d685
hkstwk <hkolvoort+github@gmail.com>
2021-01-20 23:15:35 +0100

    302. Adding Logout, including reverse pipe

---
+++ src/app/recipes/recipe-item/reverse.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'reverse'
+})
+export class ReversePipe implements PipeTransform {
+
+  transform(value: any): any {
+    let reverseCharArray : string[] = [];
+    for (let i=value.length-1; i>=0;i--){
+      reverseCharArray.push(value.charAt(i));
+    }
+    return reverseCharArray.join('');
+  }
+}
