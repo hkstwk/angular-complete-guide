af43bc85f7d77ad1252c2fd2ec70367450898cdf
hkstwk <hkolvoort+github@gmail.com>
2021-01-28 23:14:42 +0100

    328. Adding an Auth Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,33 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {RecipeModule} from "./recipes/recipe.module";
 import {AppRoutingModule} from "./app-routing.module";
 import {HttpClientModule} from "@angular/common/http";
 import {AppComponent} from "./app.component";
-import {WarningAlertComponent} from "./warningalert/warningalert.component";
-import {SuccessAlertComponent} from "./successalert/successalert.component";
 import {HeaderComponent} from "./header/header.component";
-import {AuthComponent} from "./auth/auth.component";
 import {ShoppingListModule} from "./shoppinglist/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
 import {CoreModule} from "./core.module";
+import {AuthModule} from "./auth/auth.module";
+import {SuccessAlertComponent} from "./successalert/successalert.component";
+import {WarningAlertComponent} from "./warningalert/warningalert.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
     HeaderComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipeModule,
     ShoppingListModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from "@angular/core";
+import {RouterModule} from "@angular/router";
+
+import {AuthComponent} from "./auth.component";
+import {FormsModule} from "@angular/forms";
+import {SharedModule} from "../shared/shared.module";
+
+@NgModule({
+  declarations: [
+    AuthComponent,
+  ],
+  imports: [
+    FormsModule,
+    SharedModule,
+    RouterModule.forChild([
+      {path: 'auth', component: AuthComponent}
+    ])
+  ],
+  exports: []
+})
+export class AuthModule {
+
+}
