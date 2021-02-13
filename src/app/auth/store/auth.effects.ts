9f9ab5a7351a1c3bd16ea0032bef1018556791e4
hkstwk <hkolvoort+github@gmail.com>
2021-02-13 21:11:21 +0100

    370. Finishing the Login Effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 import { Actions, Effect, ofType} from "@ngrx/effects";
 import { of } from "rxjs";
-import { catchError, map, switchMap } from "rxjs/operators";
+import { catchError, map, switchMap, tap } from "rxjs/operators";
 import { environment } from "src/environments/environment";
 import * as AuthActions from "./auth.actions";
 
@@ -38,23 +39,46 @@ export class AuthEffects {
             const expirationDate = new Date(
               new Date().getTime() + +respData.expiresIn * 1000
             );
-            of(
-              new AuthActions.Login({
-                email: respData.email,
-                userId: respData.localId,
-                token: respData.idToken,
-                expirationDate: expirationDate,
-              })
-            );
+            return new AuthActions.Login({
+              email: respData.email,
+              userId: respData.localId,
+              token: respData.idToken,
+              expirationDate: expirationDate,
+            });
           }),
-          catchError((error) => {
-            // ...
-            console.log("loging failed!");
-            return of();
+          catchError((errorRes) => {
+            let errorMessage = "An unknown error occured";
+            if (!errorRes.error || !errorRes.error.error) {
+              return of(new AuthActions.LoginFailed(errorMessage));
+            }
+            switch (errorRes.error.error.message) {
+              case "EMAIL_EXISTS":
+                errorMessage = "Email already exists";
+                break;
+              case "EMAIL_NOT_FOUND":
+                errorMessage = "Invalid email";
+                break;
+              case "INVALID_PASSWORD":
+                errorMessage = "Invalid password";
+                break;
+              case "USER_DISABLED":
+                errorMessage = "User is disabled";
+            }
+            return of(new AuthActions.LoginFailed(errorMessage));
           })
         );
     })
   );
 
-  constructor(private actions$: Actions, private http: HttpClient) {}
+  @Effect({ dispatch: false })
+  authSuccess = this.actions$.pipe(
+    ofType(AuthActions.LOGIN),
+    tap(() => this.router.navigate(["/"]))
+  );
+
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 }
