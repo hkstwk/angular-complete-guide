55d1072907519cde605753ee529ff04590dceb95
hkstwk <hkolvoort+github@gmail.com>
2021-02-15 20:46:46 +0100

    378. The Router Store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import * as fromApp from "./store/app.reducer";
 import { environment } from "src/environments/environment";
 import { EffectsModule } from "@ngrx/effects";
 import { AuthEffects } from "./auth/store/auth.effects";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -22,6 +23,7 @@ import { AuthEffects } from "./auth/store/auth.effects";
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
     StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
     AppRoutingModule,
     SharedModule,
     CoreModule,
