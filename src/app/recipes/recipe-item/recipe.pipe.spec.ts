f5c4d12ebf2b31f3423e52af2d2ef6c57e7d0fb8
Harm Kolvoort <hkolvoort@gmail.com>
2021-06-07 23:19:58 +0200

    fixed brokten test

--- src/app/recipes/recipe-item/recipe.pipe.spec.ts
+++ src/app/recipes/recipe-item/recipe.pipe.spec.ts
@@ -3,6 +3,6 @@ import { ReversePipe } from './reverse.pipe';
 describe('ReversePipe', () => {
   it('should reverse the string', () => {
     const reversePipe: ReversePipe = new ReversePipe();
-    expect(reversePipe.transform('harm')).toEqual('mrh');
+    expect(reversePipe.transform('harm')).toEqual('mrah');
   });
 });
